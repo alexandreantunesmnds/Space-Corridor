a3cf03b4104d2f109827ce32477b069df99a4cc4
Alexandre Antunes Mendes <alexandre.antunes4@etu.univ-lorraine.frgit config --global user.email alexandre.antunes4@etu.univ-lorraine.frgit config --global user.email alexandre.antunes4@etu.univ-lorraine.fr>
2021-04-15 18:34:58 +0200

    supression de la documentation html/doxygen

--- projet/html/search/all_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['finish_5fline_11',['finish_line',['../structtextures__s.html#a2c07ebdbcc6a666ba06e223d9c5c9f10',1,'textures_s::finish_line()'],['../structworld__s.html#af2b60a4e7c8f4a050080847d10fd53cd',1,'world_s::finish_line()']]],
-  ['finish_5fline_5fheight_12',['FINISH_LINE_HEIGHT',['../main_8c.html#a5da0558f0bfdf2f2133fd9178aa2cec6',1,'main.c']]]
-];
--- projet/html/search/variables_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['w_89',['w',['../structsprite__s.html#af2389d89553f12884d2b690dcc458e28',1,'sprite_s']]]
-];
